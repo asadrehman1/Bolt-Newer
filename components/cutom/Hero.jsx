95e6693df5c8552344b7dd82eb093ce6fcbcbed9
asadrehman1 <asadrehman7890661@gmail.com>
2025-08-30 23:17:51 +0500

    Added SignIn Dialog

--- components/cutom/Hero.jsx
+++ components/cutom/Hero.jsx
@@ -27,7 +27,7 @@ function Hero() {
   };
 
   return (
-    <div className="flex flex-col items-center mt-36 xl:mt-52 gap-2">
+    <div className="flex flex-col items-center mt-36 xl:mt-52 gap-2 sm:px-4">
       <h2 className="font-bold text-4xl">{Lookup.HERO_HEADING}</h2>
       <p className="text-gray-400 font-medium">{Lookup.HERO_DESC}</p>
 
--- components/cutom/SignInDialog.jsx
+++ components/cutom/SignInDialog.jsx
@@ -2,22 +2,32 @@ import React from "react";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
+import Lookup from "@/data/Lookup";
+import { Button } from "../ui/button";
 
 function SignInDialog({ open, onClose }) {
   return (
     <Dialog open={open} onOpenChange={onClose}>
-      <DialogContent>
+      <DialogContent className="sm:max-w-[600px] w-[90%] h-[350px]">
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
+          <DialogTitle />
+          <div className="flex flex-col items-center justify-center gap-3 h-full">
+            <p className="font-bold text-center text-2xl text-white">
+              {Lookup.SIGNIN_HEADING}
+            </p>
+            <p className="mt-2 text-center text-gray-400">
+              {Lookup.SIGNIN_SUBHEADING}
+            </p>
+            <Button className="bg-blue-500 text-white hover:bg-blue-400 my-2">
+              Sign In with Google
+            </Button>
+            <p className="text-xs text-gray-400">
+              {Lookup.SIGNIn_AGREEMENT_TEXT}
+            </p>
+          </div>
         </DialogHeader>
       </DialogContent>
     </Dialog>
